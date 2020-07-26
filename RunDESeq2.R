4b488a6fd81a8817759e4e43acb954cc54cb6b75
Fabio D'Orazio <34645834+fabiodorazio@users.noreply.github.com>
2020-07-26 16:41:44 +0100

    Update RunDESeq2.R

--- RunDESeq2.R
+++ RunDESeq2.R
@@ -12,9 +12,7 @@ run.deseq.genes <- function(countdata = countdata, stage, type){
   
   dds <- DESeqDataSetFromMatrix(countData = countdata, colData = coldata, design = ~ stage + condition)
   dds <- DESeq(dds)
-  normalizedData <- counts(dds, normalized = TRUE) # divides the dds by the size factor. holds the counts in a matrix. Normalized = T devides by the norm factor
-  head(normalizedData)
-  dds$condition <- factor(dds$condition, levels = c("s128","s1k")) # tells deseq what is the control. Most of times not necessery
+  normalizedData <- counts(dds, normalized = TRUE)
   
   res <- results(dds) 
   ## for testing logFC above or belove a threshold:
